296712a410c6446e4b75502563f82bf3fdc80c21
Kaique Marques <45697402+KaiqueMP@users.noreply.github.com>
2021-09-27 23:26:33 +0000

    Bug do Replit

--- js/app.js
+++ js/app.js
@@ -2,18 +2,18 @@ const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
 
-function closeMenu(){
-    navbar.classList.remove("open");
-    document.body.classList.remove("stop-scrolling");
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
 }
 
 hamburger_menu.addEventListener("click", () => {
-    if (!navbar.classList.contains("open")) {
-      navbar.classList.add("open");
-      document.body.classList.add("stop-scrolling");
-    } else {
-      closeMenu();
-    }
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
 
-    links.forEach((link) => link.addEventListener("click", () => closeMenu()))
-  });
+  links.forEach((link) => link.addEventListener("click", () => closeMenu()))
+});

0a7d6ccd4ed4990a4f62d3b6add49b2481b434fc
Kaique Marques <45697402+KaiqueMP@users.noreply.github.com>
2021-09-29 15:08:30 +0000

    - Correções no responsivo
    - Teste da tag no dragable na imagem

--- js/app.js
+++ js/app.js
@@ -23,50 +23,50 @@ hamburger_menu.addEventListener("click", () => {
 
 window.sr = ScrollReveal();
 
+
  sr.reveal('.column-1', {
   duration: 3000,
   origin: 'left',
-  distance: '100px'
+  distance: '100px',
  });
 
  sr.reveal('.column-2', {
+   delay: 500,
   duration: 3000,
-  origin: 'right',
-  distance: '100px'
  });
 
  sr.reveal('.client', {
   duration: 3000,
   origin: 'top',
-  distance: '50px'
+  distance: '50px', 
  });
 
  sr.reveal('.projetos', {
   duration: 3000,
   origin: 'top',
-  distance: '50px'
+  distance: '50px', 
  });
 
  sr.reveal('.depoimentos', {
-  duration: 3000
+  duration: 3000, 
  });
 
  sr.reveal('.card-1', {
   duration: 3000,
   origin: 'top',
-  distance: '50px'
+  distance: '50px', 
  });
 
  sr.reveal('.card-2', {
   delay: 500,
   duration: 3000,
   origin: 'top',
-  distance: '50px'
+  distance: '50px', 
  });
 
  sr.reveal('.card-3', {
   delay: 1000,
   duration: 3000,
   origin: 'top',
-  distance: '50px'
+  distance: '50px', 
  });
